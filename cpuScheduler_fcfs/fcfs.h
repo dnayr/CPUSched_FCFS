b1d1408615ef5aa989afb1a8b9c3f425ceefed97
dnayr <d-nayr@outlook.com>
2017-03-09 19:03:03 -0500

    Add project files.

---
+++ cpuScheduler_fcfs/fcfs.h
@@ -0,0 +1,13 @@
+#pragma once
+//#include <>
+
+class FCFS {
+
+	FCFS() {
+
+	}
+
+	void doInsertProcess() {
+		// 
+	}
+};
---
+++ cpuScheduler_fcfs/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+// TODO: fn to find TT (turnaround time)
+// TODO: fn to find TW (wait time)
+// TODO; response time
+// TODO: cpu util
+
+int main() 
+{
+	queue<int> fcfs;
+
+
+
+	return 0;
+}
